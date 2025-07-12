8ad25b83f13269c7f035921f09d679b1065f9410
Vishnu Vamsith Yejju <yejju.v_int@external.swiggy.in>
2025-07-12 11:05:28 +0530

    production fixes

--- server/app.js
+++ server/app.js
@@ -14,6 +14,10 @@ app.use('/crud',crud)
 app.use('/attendance',attendance)
 app.use('/payroll',payroll)
 app.use('/atten',attencrud)
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 app.listen(PORT, () => {
   console.log(`✅ Server listening on http://localhost:${PORT}`);
 });
